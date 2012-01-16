b405532da0786211f734e6de542110b1c03b1036
Grant Rodgers <grantr@gmail.com>
2012-01-15 19:20:51 -0800

    explain what to do next
    Give the user some direction after using the generator.
    
    Fixes #3

--- lib/springboard/generators/config_generator.rb
+++ lib/springboard/generators/config_generator.rb
@@ -10,6 +10,9 @@ module Springboard
         template "elasticsearch.yml", "config/elasticsearch/elasticsearch.yml"
         copy_file "logging.yml", "config/elasticsearch/logging.yml"
         copy_file "elasticsearch.in.sh", "config/elasticsearch/elasticsearch.in.sh"
+        say "Run elasticsearch with springboard -c config/elasticsearch to use your new config."
+        say "Check here for more configuration options:"
+        say "  https://github.com/elasticsearch/elasticsearch/blob/master/config/elasticsearch.yml"
       end
 
 
